544150d12c23f8a7a398e8c60f8a0791de3ec35d
FlandreDaisuki <vbnm123c@gmail.com>
2019-03-21 02:34:36 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const $ = (s, doc = document) => {
+  return doc.querySelector(s);
+};
+
+const $$ = (s, doc = document) => {
+  return [...doc.querySelectorAll(s)];
+};
+
+const $el = (tag, attr = {}, cb = () => {}) => {
+  const el = document.createElement(tag);
+  Object.assign(el, attr);
+  cb(el);
+  return el;
+};
+
+const render = (str, pos) => {
+  let result = '';
+  for (let i = 0; i < pos.length - 1; ++i) {
+      const substr = str.slice(pos[i], pos[i + 1]);
+      const match = substr.match(/^([^(]+)\(([^)]+)\)$/);
+      console.log(substr, match);
+      if (match) {
+          result += `<ruby>${match[1]}<rp>(</rp><rt>${match[2]}</rt><rp>)</rp></ruby>`;
+      } else {
+          result += substr;
+      }
+  }
+  return `<div class="lyric-row">${result}</div>`;
+};
+
+const process = () => {
+  const lines = $('#text-editor + label + .tab-content pre').textContent
+      .split('\n')
+      .map(r => r.trim());
+
+  const rendered = [];
+  for (const line of lines) {
+      /* 平仮名 3041-3093 */
+      /* 片仮名 30A0-30FF*/
+      const lineIter = line.matchAll(/[^\u{3041}-\u{3093}\u{30A0}-\u{30FF}\s「」？?、]+\([^)]+\)/gu);
+      const collect = new Set([0, line.length]);
+      for (const wordAnchor of lineIter) {
+          collect.add(wordAnchor.index);
+          collect.add(wordAnchor.index + wordAnchor[0].length);
+      }
+      const position = [...collect].sort((a, b) => a - b);
+      // console.log(position);
+      rendered.push(render(line, position));
+  }
+  console.log(rendered);
+  const title = $('#text-editor + label + .tab-content h1');
+  $('#ruby-rendered-lyrics').innerHTML = title.outerHTML + rendered.join('');
+}
+
+const toFullHTML = (html) => {
+  const title = $('#ruby-rendered-lyrics h1').textContent;
+  return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <meta charset="UTF-8">
+          <title>${title}</title>
+      </head>
+      <body>
+          ${html}
+      </body>
+      </html>
+  `.split('\n').map(l => l.trim()).join('\n');
+}
+
+$('#ruby-renderer').addEventListener('change', process);
+
+new ClipboardJS('#copy-btn', {
+  text: () => $('#ruby-rendered-lyrics').outerHTML,
+});
+
+$('#download-btn').addEventListener('click',() => {
+  const blob = new Blob([toFullHTML($('#ruby-rendered-lyrics').outerHTML)], {type: 'text/html;charset=utf-8'});
+  const title = $('#ruby-rendered-lyrics h1').textContent;
+  saveAs(blob, `${title}.html`.replace(/\//g, '／'));
+});
